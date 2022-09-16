b33ac7eaa85536cd093ae060a09dde33c168d48c
matthias <matthias.deruddere@gmail.com>
2022-09-16 08:31:45 +0200

    vreemde accolade verwijderd

--- src/pages/menuPage/menuPage.tsx
+++ src/pages/menuPage/menuPage.tsx
@@ -55,7 +55,7 @@ const MenuPage: React.FC<Props> = ({ consumptionCollection }) => {
           Een drankkaart kost <b>12 euro</b>, vraag er gerust eentje aan de bar.
           <br />
           Bestellen kan - je raad het nooit - aan de bar. Dus als ge daar dan toch staat voor een kaart, kunt ge
-          evengoed wat vakjes laten doorstrepen hé.{' '}
+          evengoed wat vakjes laten doorstrepen hé.
         </p>
         <p>Kaarten die worden aangekocht met andere drankkaarten zijn om ecologische redenen 17 euro duurder.</p>
         <img style={{ maxWidth: 200 }} src="https://despelmaeckers.be/images/event-logo.png"></img>
